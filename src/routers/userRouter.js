9e498d7f2a6307eca7346e7b80c5556a3133b551
andyyyho <andyho@ucsb.edu>
2022-05-21 11:50:41 -0700

    Added routes for get all bodyStats and create bodyStat

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { deleteUser, updateUser, getUser, registerUser, loginUser } = require('../controllers/userController')
 const auth = require('../middleware/authMiddleware')
+const BodyStatController = require('../controllers/bodyStatController')
+const asyncHandler = require('express-async-handler')
 
 
 router.post('/register', registerUser)
@@ -13,5 +15,9 @@ router.get('/profile', auth, getUser)
 router.patch('/edit', auth, updateUser)
 
 router.delete('/delete', auth, deleteUser)
+
+router.post('/bodyStats', auth, asyncHandler(BodyStatController.addBodyStat))
+
+router.get('/bodyStats', auth, asyncHandler(BodyStatController.getBodyStats))
 
 module.exports = router
