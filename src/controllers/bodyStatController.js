004986d02e1e21553f8c98679cb61bfd4d5f4061
andyyyho <andyho@ucsb.edu>
2022-05-23 22:35:10 -0700

    Added additional CRUD operation endpoints and controller for body stats

--- src/controllers/bodyStatController.js
+++ src/controllers/bodyStatController.js
@@ -4,25 +4,62 @@ const BodyStat = require('../schemas/bodyStatSchema')
 
 let BodyStatController = {
     addBodyStat: async (req, res) => {
-        let user = res.locals.user
+        const user = res.locals.user
         const requestData = {owner: user}
         if (req.body.bodyFat) requestData.bodyFat = req.body.bodyFat
         if (req.body.weight) requestData.weight = req.body.weight
         const stat = await BodyStat.create(requestData)
         user.bodyStats.push(stat)
         await user.save()
+
         res.send(user)
     },
     
-    updateBodyStat: asyncHandler( async (req, res) => {
-        
-    }),
+    updateBodyStat: async (req, res) => {
+        const stat = await BodyStat.findById(req.params.bodyStatID)
+        const validUpdates = ['weight', 'bodyFat']
+        const updates = Object.keys(req.body)
+        const validAction = updates.every(element => {
+            if (validUpdates.includes(element)) return true
+        })
+        if (validAction && res.locals.user.id == stat.owner){
+            updates.forEach(element => {
+                stat[element] = req.body[element]
+            })
+            await stat.save()
+            res.send(stat)
+        }
+        else{
+            res.status(401).send({error: "Unable to update this body stat."})
+        } 
+    },
     
-    removeBodyStat: asyncHandler( async (req, res) => {
-        
-    }),
+    removeBodyStat: async (req, res) => {
+        const stat = await BodyStat.findById(req.params.bodyStatID)
+        const user = res.locals.user
+        const userBodyStats = (await user.populate('bodyStats')).bodyStats
+
+        if (res.locals.user.id == stat.owner){
+            const userStats = userBodyStats.filter((element) => {
+                return (element.id.toString() !== stat.id)
+            })
+            user.bodyStats = userStats
+            await user.save()
+            await BodyStat.deleteOne({id: stat.id})    
+            res.send({message: "Succesfully deleted body statistic.", user})
+        } 
+        else {
+            res.status(401).send({error: "Unable to access this body statistic."})
+        }
+    },
+
+    getBodyStat: async (req, res) => {
+        const stat = await BodyStat.findById(req.params.bodyStatID)
+        if (res.locals.user.id == stat.owner) res.send(stat)
+        else res.status(404).send({error: "Unable to locate body statistic."})
+    },
     
-    getBodyStats: asyncHandler( async (req, res) => {
+    getBodyStats: async (req, res) => {
         const user = res.locals.user
         const bodyStats = (await user.populate('bodyStats')).bodyStats
         const cleanData = []
@@ -33,8 +70,8 @@ let BodyStatController = {
                 createdAt: element.createdAt
             })
         })
-        res.send(cleanData)
-    })
+        res.send(bodyStats)
+    }
 }
 
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/authMiddleware')
 const BodyStatController = require('../controllers/bodyStatController')
 const asyncHandler = require('express-async-handler')
 
+// Regular user routes
 
 router.post('/register', registerUser)
 
@@ -16,8 +17,18 @@ router.patch('/edit', auth, updateUser)
 
 router.delete('/delete', auth, deleteUser)
 
+// Body statistic routes
+
 router.post('/bodyStats', auth, asyncHandler(BodyStatController.addBodyStat))
 
 router.get('/bodyStats', auth, asyncHandler(BodyStatController.getBodyStats))
+
+router.get('/bodyStats/:bodyStatID', auth, asyncHandler(BodyStatController.getBodyStat))
+
+router.patch('/bodyStats/:bodyStatID', auth, asyncHandler(BodyStatController.updateBodyStat))
+
+router.delete('/bodyStats/:bodyStatID', auth, asyncHandler(BodyStatController.removeBodyStat))
+
+
 
 module.exports = router
