c2bdb56f9515abdb69fbb1371f5e08eacde76946
andyyyho <andyho@ucsb.edu>
2022-07-07 23:34:59 -0700

    AH/DW - Added workout schema to store user workout sessions

---
+++ src/schemas/workoutSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const workoutSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    },
+    lifts: [{
+        type: Schema.Types.ObjectId,
+        ref: "liftEntry"
+    }],
+    notes: {
+        type: String
+    }
+
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('workout', workoutSchema)
