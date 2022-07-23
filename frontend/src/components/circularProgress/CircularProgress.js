2f96a24df8402da6f8ef9133bfade1d64c71b3c0
andyyyho <andyho@ucsb.edu>
2022-07-23 00:51:08 -0700

    Created component to display number of days gone in the current week

---
+++ frontend/src/components/circularProgress/CircularProgress.js
@@ -0,0 +1,29 @@
+import './CircularProgress.scss'
+
+function CircularProgress(props) {
+
+    
+    return (
+        <div className='circ-container'>
+            <div className='outer-ring'>
+                <div className='inner-ring'>
+                    <div id='progress-num'>
+                        {props.value + ' ' + props.label}
+                    </div>
+                </div>
+            </div>
+
+            <svg width="100%" height="100%" viewBox="0 0 160 160">
+                <defs>
+                    <linearGradient id='linear-prog-gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
+                        <stop offset='0%' stop-color='#323cff'/>
+                        <stop offset='100%' stop-color='#b867ff'/>
+                    </linearGradient>
+                </defs>
+                <circle className={'circle-' + props.value}  cx="80" cy="80" r="70" />
+            </svg>
+        </div>
+    )
+}
+
+export default CircularProgress;
