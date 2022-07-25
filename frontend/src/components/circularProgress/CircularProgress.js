12eb736da8a9acbf70b75413301a075fd97539bf
andyyyho <andyho@ucsb.edu>
2022-07-24 23:30:54 -0700

    Changed property to remove error

--- frontend/src/components/circularProgress/CircularProgress.js
+++ frontend/src/components/circularProgress/CircularProgress.js
@@ -16,8 +16,8 @@ function CircularProgress(props) {
             <svg width="100%" height="100%" viewBox="0 0 160 160">
                 <defs>
                     <linearGradient id='linear-prog-gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
-                        <stop offset='0%' stop-color='#323cff'/>
-                        <stop offset='100%' stop-color='#b867ff'/>
+                        <stop offset='0%' stopColor='#323cff'/>
+                        <stop offset='100%' stopColor='#b867ff'/>
                     </linearGradient>
                 </defs>
                 <circle className={'circle-' + props.value}  cx="80" cy="80" r="70" />
